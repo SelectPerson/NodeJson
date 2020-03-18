c2243ffd9a098337f7cc8211d051dce5d274b190
SelectPerson <test@gmail.com>
2020-03-18 15:40:46 +0200

    More

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+const PORT = proccess.env.PORT || 3000;
+
 app.get('/', (req,res) => {
    res.send('<h1>Home page</h1>');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Nice');
 });
 
